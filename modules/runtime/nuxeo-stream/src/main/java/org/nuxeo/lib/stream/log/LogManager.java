860264de5b03ee7dbdace6dea1303634d9401379
Benoit Delbosc <bdelbosc@nuxeo.com>
2020-04-06 19:53:21 +0200

    NXP-28877: Use Name object for stream and consumer group

--- modules/runtime/nuxeo-stream/src/main/java/org/nuxeo/lib/stream/log/LogManager.java
+++ modules/runtime/nuxeo-stream/src/main/java/org/nuxeo/lib/stream/log/LogManager.java
@@ -42,26 +42,26 @@ public interface LogManager extends AutoCloseable {
     /**
      * Returns {@code true} if a Log with this {@code name} exists.
      */
-    boolean exists(String name);
+    boolean exists(Name name);
 
     /**
      * Creates a new Log with {@code size} partitions if the Log does not exists. Returns true it the Log has been
      * created.
      */
-    boolean createIfNotExists(String name, int size);
+    boolean createIfNotExists(Name name, int size);
 
     /**
      * Tries to delete a Log. Returns true if successfully deleted, might not be possible depending on the
      * implementation.
      */
-    boolean delete(String name);
+    boolean delete(Name name);
 
     /**
      * Returns the number of partition of a Log.
      *
      * @since 10.2
      */
-    int size(String name);
+    int size(Name name);
 
     /**
      * Gets an appender for the Log named {@code name}, uses {@code codec} to encode records. An appender is thread
@@ -69,13 +69,13 @@ public interface LogManager extends AutoCloseable {
      *
      * @since 10.2
      */
-    <M extends Externalizable> LogAppender<M> getAppender(String name, Codec<M> codec);
+    <M extends Externalizable> LogAppender<M> getAppender(Name name, Codec<M> codec);
 
     /**
      * Gets an appender for the Log named {@code name}, uses an already defined codec or the legacy encoding to encode
      * records. An appender is thread safe.
      */
-    default <M extends Externalizable> LogAppender<M> getAppender(String name) {
+    default <M extends Externalizable> LogAppender<M> getAppender(Name name) {
         return getAppender(name, NO_CODEC);
     }
 
@@ -85,7 +85,7 @@ public interface LogManager extends AutoCloseable {
      *
      * @since 10.2
      */
-    <M extends Externalizable> LogTailer<M> createTailer(String group, Collection<LogPartition> partitions,
+    <M extends Externalizable> LogTailer<M> createTailer(Name group, Collection<LogPartition> partitions,
             Codec<M> codec);
 
     /**
@@ -93,7 +93,7 @@ public interface LogManager extends AutoCloseable {
      * {@code partitions} can be from different Logs. Reads records using the legacy decoder. A tailer is NOT thread
      * safe.
      */
-    default <M extends Externalizable> LogTailer<M> createTailer(String group, Collection<LogPartition> partitions) {
+    default <M extends Externalizable> LogTailer<M> createTailer(Name group, Collection<LogPartition> partitions) {
         return createTailer(group, partitions, NO_CODEC);
     }
 
@@ -101,7 +101,7 @@ public interface LogManager extends AutoCloseable {
      * Creates a tailer for a consumer {@code group} and assign a single {@code partition}. Reads records using the
      * legacy decoder. A tailer is NOT thread safe.
      */
-    default <M extends Externalizable> LogTailer<M> createTailer(String group, LogPartition partition) {
+    default <M extends Externalizable> LogTailer<M> createTailer(Name group, LogPartition partition) {
         return createTailer(group, partition, NO_CODEC);
     }
 
@@ -109,7 +109,7 @@ public interface LogManager extends AutoCloseable {
      * Creates a tailer for a consumer {@code group} and assign all {@code partitions} of the Log. Reads records using
      * the legacy decoder. A tailer is NOT thread safe.
      */
-    default <M extends Externalizable> LogTailer<M> createTailer(String group, String name) {
+    default <M extends Externalizable> LogTailer<M> createTailer(Name group, Name name) {
         return createTailer(group, name, NO_CODEC);
     }
 
@@ -119,7 +119,7 @@ public interface LogManager extends AutoCloseable {
      *
      * @since 10.2
      */
-    default <M extends Externalizable> LogTailer<M> createTailer(String group, LogPartition partition, Codec<M> codec) {
+    default <M extends Externalizable> LogTailer<M> createTailer(Name group, LogPartition partition, Codec<M> codec) {
         return createTailer(group, Collections.singletonList(partition), codec);
     }
 
@@ -129,7 +129,7 @@ public interface LogManager extends AutoCloseable {
      *
      * @since 10.2
      */
-    default <M extends Externalizable> LogTailer<M> createTailer(String group, String name, Codec<M> codec) {
+    default <M extends Externalizable> LogTailer<M> createTailer(Name group, Name name, Codec<M> codec) {
         int partitions = size(name);
         if (partitions <= 0) {
             throw new IllegalArgumentException("Log name: " + name + " not found");
@@ -156,10 +156,10 @@ public interface LogManager extends AutoCloseable {
      * <p/>
      * You should not mix {@link #createTailer} and {@code subscribe} usage using the same {@code group}.
      */
-    <M extends Externalizable> LogTailer<M> subscribe(String group, Collection<String> names,
+    <M extends Externalizable> LogTailer<M> subscribe(Name group, Collection<Name> names,
             RebalanceListener listener, Codec<M> codec);
 
-    default <M extends Externalizable> LogTailer<M> subscribe(String group, Collection<String> names,
+    default <M extends Externalizable> LogTailer<M> subscribe(Name group, Collection<Name> names,
             RebalanceListener listener) {
         return subscribe(group, names, listener, NO_CODEC);
     }
@@ -168,24 +168,23 @@ public interface LogManager extends AutoCloseable {
      * Returns the lag between consumer {@code group} and the producers for each partition. The result list is ordered,
      * for instance index 0 is lag for partition 0.
      */
-    List<LogLag> getLagPerPartition(String name, String group);
+    List<LogLag> getLagPerPartition(Name name, Name group);
 
     /**
      * Returns the lag between consumer {@code group} and producers for a Log.
      */
-    default LogLag getLag(String name, String group) {
+    default LogLag getLag(Name name, Name group) {
         return LogLag.of(getLagPerPartition(name, group));
     }
 
     /**
      * Returns the lag with latency. Timestamps used to compute the latencies are extracted from the records. This
-     * requires to read one record per partition so it costs more than {@link #getLagPerPartition(String, String)}.
-     * <br/>
+     * requires to read one record per partition so it costs more than {@link #getLagPerPartition(Name, Name)}. <br/>
      * Two functions need to be provided to extract the timestamp and a key from a record.
      *
      * @since 10.2
      */
-    <M extends Externalizable> List<Latency> getLatencyPerPartition(String name, String group, Codec<M> codec,
+    <M extends Externalizable> List<Latency> getLatencyPerPartition(Name name, Name group, Codec<M> codec,
             Function<M, Long> timestampExtractor, Function<M, String> keyExtractor);
 
     /**
@@ -193,48 +192,21 @@ public interface LogManager extends AutoCloseable {
      *
      * @since 10.2
      */
-    default <M extends Externalizable> Latency getLatency(String name, String group, Codec<M> codec,
+    default <M extends Externalizable> Latency getLatency(Name name, Name group, Codec<M> codec,
             Function<M, Long> timestampExtractor, Function<M, String> keyExtractor) {
         return Latency.of(getLatencyPerPartition(name, group, codec, timestampExtractor, keyExtractor));
     }
 
     /**
-     * Returns the lag with latency. Timestamps used to compute the latencies are extracted from the records. This
-     * requires to read one record per partition so it costs more than {@link #getLagPerPartition(String, String)}.
-     * <br/>
-     * Two functions need to be provided to extract the timestamp and a key from a record.
-     *
-     * @since 10.1
-     * @deprecated 10.2 use {@link #getLatencyPerPartition(String, String, Codec, Function, Function)} instead.
-     */
-    @Deprecated
-    default <M extends Externalizable> List<Latency> getLatencyPerPartition(String name, String group,
-            Function<M, Long> timestampExtractor, Function<M, String> keyExtractor) {
-        return getLatencyPerPartition(name, group, NO_CODEC, timestampExtractor, keyExtractor);
-    }
-
-    /**
-     * Returns the latency between consumer {@code group} and producers for a Log.
-     *
-     * @since 10.1
-     * @deprecated 10.2 use {@link #getLatency(String, String, Codec, Function, Function)} instead.
-     */
-    @Deprecated
-    default <M extends Externalizable> Latency getLatency(String name, String group,
-            Function<M, Long> timestampExtractor, Function<M, String> keyExtractor) {
-        return getLatency(name, group, NO_CODEC, timestampExtractor, keyExtractor);
-    }
-
-    /**
      * Returns all the Log names.
      */
-    List<String> listAll();
+    List<Name> listAll();
 
     /**
      * List the consumer groups for a Log.<br/>
      * Note that for Kafka it returns only consumers that use the subscribe API.
      */
-    List<String> listConsumerGroups(String name);
+    List<Name> listConsumerGroups(Name name);
 
     @Override
     void close();
---
+++ modules/runtime/nuxeo-stream/src/main/java/org/nuxeo/lib/stream/log/Name.java
@@ -0,0 +1,148 @@
+/*
+ * (C) Copyright 2020 Nuxeo (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     bdelbosc
+ */
+package org.nuxeo.lib.stream.log;
+
+import java.util.Objects;
+import java.util.regex.Pattern;
+
+/**
+ * @since 11.1
+ */
+public class Name {
+    public static final String NAMESPACE_EMPTY = "_EMPTY_";
+
+    public static final String NAMESPACE_URN_SEP = "/";
+
+    public static final String NAMESPACE_ID_SEP = "-";
+
+    // we don't want NAMESPACE_ID_SEP here
+    protected static final Pattern VALID_NAMESPACE_PATTERN = Pattern.compile("[A-Za-z][A-Za-z0-9_]*");
+
+    protected static final Pattern VALID_LOG_NAME_PATTERN = Pattern.compile("[A-Za-z0-9][A-Za-z0-9_\\-]*");
+
+    protected static final Pattern VALID_LOG_NAME_WITHOUT_NS_PATTERN = Pattern.compile("[A-Za-z0-9][A-Za-z0-9_]*");
+
+    protected final String namespace;
+
+    protected final String name;
+
+    protected final String id;
+
+    protected final String urn;
+
+    private Name(String namespace, String name) {
+        checkNameSpace(namespace);
+        checkLogName(name);
+        this.namespace = namespace;
+        this.name = name;
+        if (NAMESPACE_EMPTY.equals(namespace)) {
+            checkLogNameWithoutNamespace(name);
+            this.id = name;
+            this.urn = name;
+        } else {
+            this.id = namespace + NAMESPACE_ID_SEP + name;
+            this.urn = namespace + NAMESPACE_URN_SEP + name;
+        }
+    }
+
+    public static Name of(String namespace, String name) {
+        return new Name(namespace, name);
+    }
+
+    public static Name ofUrn(String urn) {
+        Objects.requireNonNull(urn, "Null URN");
+        int pos = urn.indexOf(NAMESPACE_URN_SEP);
+        if (pos < 0) {
+            return new Name(NAMESPACE_EMPTY, urn);
+        }
+        return new Name(urn.substring(0, pos), urn.substring(pos + 1));
+    }
+
+    public static Name ofId(String id) {
+        Objects.requireNonNull(id, "Null id");
+        int pos = id.indexOf(NAMESPACE_ID_SEP);
+        if (pos < 0) {
+            return new Name(NAMESPACE_EMPTY, id);
+        }
+        return new Name(id.substring(0, pos), id.substring(pos + 1));
+    }
+
+    public static String idOfUrn(String urn) {
+        return Name.ofUrn(urn).getId();
+    }
+
+    public static String urnOfId(String id) {
+        return Name.ofId(id).getUrn();
+    }
+
+    public String getNamespace() {
+        return namespace;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getUrn() {
+        return urn;
+    }
+
+    @Override
+    public String toString() {
+        return "Name{" + "namespace='" + namespace + '\'' + ", name='" + name + '\'' + ", id='" + id + '\'' + ", urn='"
+                + urn + '\'' + '}';
+    }
+
+    protected static void checkLogName(String name) {
+        if (!VALID_LOG_NAME_PATTERN.matcher(name).matches()) {
+            throw new IllegalArgumentException("Invalid name: '" + name + "'.");
+        }
+    }
+
+    protected static void checkLogNameWithoutNamespace(String name) {
+        if (!VALID_LOG_NAME_WITHOUT_NS_PATTERN.matcher(name).matches()) {
+            throw new IllegalArgumentException("Invalid name without namespace: '" + name + "'");
+        }
+    }
+
+    protected static void checkNameSpace(String name) {
+        if (!VALID_NAMESPACE_PATTERN.matcher(name).matches() & !NAMESPACE_EMPTY.equals(name)) {
+            throw new IllegalArgumentException("Invalid namespace: '" + name + "'");
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Name name = (Name) o;
+        return Objects.equals(urn, name.urn);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(urn);
+    }
+}
